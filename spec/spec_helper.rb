dbf3200f6aa7e802da4e857a3555f53ae3e1239e
Josephine Hall <josephinehall@gmail.com>
2016-07-12 12:25:47 +1000

    Set up the spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,21 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'icy/content_buffer'
+SPEC_ROOT = __dir__
+
+# Prefer plain ERB processor rather than erubis (which has problems on JRuby)
+require "erb"
+require "tilt"
+Tilt.register "erb", Tilt::ERBTemplate
+
+require "byebug"
+require "dry-view"
+require "icy-content_buffer"
+
+RSpec.configure do |config|
+  config.disable_monkey_patching!
+
+  config.order = :random
+  Kernel.srand config.seed
+end
+
+module Test
+
+end
